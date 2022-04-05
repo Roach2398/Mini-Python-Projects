1e7b4893bd2c47e2a32581ebdbb88faf1c6ce0b4
Roach2398 <83520727+Roach2398@users.noreply.github.com>
2022-04-04 21:34:34 -0400

    Add files via upload

---
+++ Projects/Hello_World.py
@@ -0,0 +1,9 @@
+import random
+
+hw = ["Hello World", "Hallo Welt", "Ola Mundo", "Привет мир", "こんにちは世界", "헬로월드", "Hola Mundo", "Bonjour le Monde"]
+
+pick = random.randint(0,7)
+
+print(hw[pick])
+
+# Hello World in a random lanuage (English, German, Portugese, Russian, Japanese, Korean, Spanish, or French.)
